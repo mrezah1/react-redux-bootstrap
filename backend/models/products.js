f803e9f604ea84d51e551cbf24e6bf7fd70f90a3
Mohammadreza <Mrhhajiyan@gmail.com>
2021-10-01 18:23:50 +0330

    add backend to project

---
+++ backend/models/products.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Airpods',
+    image: '/images/airpods.jpg',
+    description: 'Product Description Airpods',
+    price: 99,
+  },
+  {
+    _id: '2',
+    name: 'Apple Watch',
+    image: '/images/apple-watch.jpg',
+    description: 'Product Description Apple Watch',
+    price: 79,
+  },
+  {
+    _id: '3',
+    name: 'iPhone 12',
+    image: '/images/iphone-12.jpg',
+    description: 'Product Description iPhone 12',
+    price: 89,
+  },
+  {
+    _id: '4',
+    name: 'iPhone SE',
+    image: '/images/iphone-se.jpg',
+    description: 'Product Description iPhone SE',
+    price: 29,
+  },
+  {
+    _id: '5',
+    name: 'MacBook Pro',
+    image: '/images/macbook.jpg',
+    description: 'Product Description MacBook Pro',
+    price: 89,
+  },
+  {
+    _id: '6',
+    name: 'Logitech MX Master',
+    image: '/images/mouse.jpg',
+    description: 'Product Description Logitech MX Master',
+    price: 59,
+  },
+]
+
+module.exports = products
