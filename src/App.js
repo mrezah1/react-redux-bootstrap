20e06501ed2cdfce99cb045ca34bd665ea57ac09
Mohammadreza <Mrhhajiyan@gmail.com>
2021-10-01 18:20:38 +0330

    add route and component to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h2>Custom Market</h2>
+          <Route exact path="/" component={Home} />
+          <Route path="/product/:id" component={ProductDetail} />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
