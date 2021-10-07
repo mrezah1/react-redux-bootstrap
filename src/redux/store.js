62a707fc3aad82262ffa9d7e532e05c63d39ca8f
Mohammadreza <Mrhhajiyan@gmail.com>
2021-10-07 03:47:03 +0330

    develop cart store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,19 @@ import {
   productListReducer,
   productDetailReducer,
 } from "./reducer/ProductReducer";
+import { cartReducer } from "./reducer/cartReducer";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetail: productDetailReducer,
+  cart: cartReducer,
 });
-const initialState = {};
+const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+const initialState = {
+  cart: { cartItems: cartItemsFromLocalStorage },
+};
 const middleWare = [thunk];
 
 const store = createStore(
